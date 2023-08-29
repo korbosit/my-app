55ce894d299e6573b26bffd37a56eceaf9543ee9
Vlaimir Korba <krissky1986@gmail.com>
2023-08-29 14:02:25 +0300

    props practice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+function WhoAmI({name, surname, link}) {
+   return (
+		<div>
+			<h1>My name is {name()}, surname - {surname}</h1>
+			<a href={link}>My profile</a>
+		</div>
+	);
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<WhoAmI name={() => {return 'John'}} surname='Smith' link='facebook.com'/>
+			<WhoAmI name={() => {return 'Alex'}} surname='Shepard' link='vk.com'/>
+		</div>
+	);
 }
 
 export default App;
